1a1698ca99fa4dcda8cd9db661363c7119033260
Maks <rubelmaksum2404@gmail.com>
2025-01-13 20:30:23 +0200

    new

---
+++ 14_Dalegate/Program.cs
@@ -0,0 +1,44 @@
+﻿
+delegate bool Condition<T>(T numb);
+
+internal class Program
+{
+   
+
+    public static int CountInArray<T>(T[] arr, Condition<T> func)
+    {
+        int count = 0;
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    if(func(i))
+        //        count++;
+        //}
+        return count;
+    }
+
+    public static T[] FilterArray<T>(T[] arr, Condition<T> func)
+    {
+        T[] arr_ = new T[0];
+        foreach(var item in arr)
+        {
+            if(func(item))
+            {
+                Array.Resize(ref arr_, arr_.Length + 1);
+                arr_[arr_.Length - 1] = item;
+            }
+        }
+        return arr_;
+    }
+
+
+
+
+    private static void Main(string[] args)
+    {
+        int[] arr = { 1, 8, -4, -5, -7, 6, 3, 5, -6 };
+        Console.WriteLine($"Number positive elements :: {CountInArray(arr,e => e > 0)}");
+        Console.WriteLine($"Number negative elements :: {CountInArray(arr, e => e < 0)}");
+
+        double[] arr2 = {1.5, -5.3, 2.3, -14.2, 2.5 };
+    }
+}
---
+++ 16_Class_Delegate/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+delegate void ShowMessage(string message);
+
+class OneShow
+{
+
+    public static void Print(string message) 
+    {
+        Console.WriteLine(message);
+    }
+}
+delegate double CalcDelegate(double one, double two);
+class Calculate
+{
+    public static double Add(double a, double b) => a + b;
+    public static double Sub(double a, double b) => a - b;
+    public static double Mult(double a, double b) => a * b;
+
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ShowMessage sms =  new ShowMessage(OneShow.Print);
+        sms("Hello World!!");
+
+        CalcDelegate calc = Calculate.Add;
+        Console.WriteLine($"Result :: {calc(2, 3)}");
+        calc += Calculate.Sub;
+        Console.WriteLine($"Result :: {calc(2, 3)}");
+        calc += Calculate.Mult;
+        Console.WriteLine($"Result :: {calc(2, 3)}");
+
+
+        Predicate<int> IsEven = number => number % 2 == 0;
+        Predicate<int> IsOdd = number => number % 2 != 0;
+        Predicate<int> IsPrime = number => isPrime(number);
+        Predicate<int> IsFibonaci = number => isFabonacci(number);
+
+
+        Console.WriteLine($"IsEven: {IsEven(22)}");
+        Console.WriteLine($"IsOdd: {IsOdd(23)}");
+        Console.WriteLine($"IsPrime: {IsPrime(97)}");
+        Console.WriteLine($"IsFibonaci: {IsFibonaci(5)}");
+
+
+    }
+
+    static bool isPrime(int number)
+    {
+        if(number<= 1) return false;
+        for(int i = 2; i <= number - 1; i++)
+        {
+            if(number % i == 0)
+                return false;
+        }
+        return true;
+    }
+    static bool isFabonacci(int number)
+    {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+      
+        while (c < number)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+
+        }
+        return c == number;
+    }
+}
