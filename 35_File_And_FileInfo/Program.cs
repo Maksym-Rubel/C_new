5422cba037e689f6c0ec5344205157ab8e4e59bd
Maks <rubelmaksum2404@gmail.com>
2025-01-27 20:41:10 +0200

    new

---
+++ 35_File_And_FileInfo/Program.cs
@@ -0,0 +1,76 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] lines = { "Line 1", "Line 2" };
+        string fname = "../../../my.txt";
+        //File.SetAttributes(fname, FileAttributes.Archive);
+        if(File.Exists(fname))
+        {
+            File.Delete(fname);
+            Console.WriteLine($"File '{fname}' was deleted");
+        }
+        File.AppendAllLines(fname, lines);
+        File.AppendAllText(fname, "Line 3\nLine 4 ");
+
+        Console.WriteLine($"Content of file '{fname}' \n{String.Join<string>("\n", File.ReadAllLines(fname))}");
+
+        string fname2 = "../../../copy_my.txt";
+        //File.Copy(fname, fname2); //exception
+
+        File.Copy(fname, fname2,true);
+
+        Console.WriteLine($"Content of file '{fname2}' \n{String.Join<string>("\n", File.ReadAllLines(fname))}");
+
+
+
+
+        //FileStream fs = File.Create(fname2);
+        StreamWriter sw = File.CreateText(fname);
+        sw.WriteLine("Write someting .... ");
+        sw.Close();
+
+
+        File.AppendAllText(fname, "New line");
+        Console.WriteLine($"Content of file '{fname}' \n{String.Join<string>("\n", File.ReadAllLines(fname))}");
+
+        Console.WriteLine($"Creation time {File.GetCreationTime(fname)}");
+        Console.WriteLine($"Last write time {File.GetLastWriteTime(fname)}");
+
+
+        string fname3 = "../../../third.txt";
+        FileInfo file = new FileInfo(fname3);
+        Console.WriteLine($"\n\n Is Exists file '{fname3}'  ::  {file.Exists}");
+
+        if(!file.Exists)
+        {
+            using(var tw = file.CreateText())
+            {
+                tw.WriteLine("It is third file");
+            }
+        }
+
+        Console.WriteLine($"FullName :: {file.FullName}");
+        Console.WriteLine($"Name :: {file.Name}");
+        Console.WriteLine($"Directrort :: {file.Directory}");
+
+        Console.WriteLine($"Content of file '{fname3}' \n{String.Join<string>("\n", File.ReadAllLines(fname))}");
+
+
+        //file.MoveTo("./third.txt");
+        //file.MoveTo("./third.txt");
+        //file.MoveTo("./" + file.Name,true);
+
+        //file.MoveTo(Path.Combine(".",file.Name),true);
+
+        Console.WriteLine($"Length     :: {file.Length}");
+        Console.WriteLine($"Extension  :: {file.Extension}");
+        Console.WriteLine($"Attributes :: {File.GetAttributes(fname)}");
+
+
+        File.SetAttributes(fname, FileAttributes.Hidden);
+        Console.WriteLine($"Attributes :: {File.GetAttributes(fname)}");
+
+
+    }
+}
---
+++ 37_class_File/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.IO;
+using System.Threading.Tasks.Dataflow;
+
+internal class Program
+{
+    static public void CreateDIrectory(string folderName)
+    {
+        if (!Directory.Exists(folderName))
+        {
+            Directory.CreateDirectory(folderName);
+            Console.WriteLine($"Folder '{folderName}' created");
+        }
+        else
+        {
+            Console.WriteLine($"Folder '{folderName}' uhe ye");
+        }
+    }
+    static public void DeleteDIrectory(string folderName)
+    {
+        if (!Directory.Exists(folderName))
+        {
+            Directory.Delete(folderName,true);
+            Console.WriteLine($"Folder '{folderName}' deleted");
+        }
+        else
+        {
+            Console.WriteLine($"Folder '{folderName}' not created");
+        }
+    }
+    static public void ChangeDirectory(string folderName)
+    {
+ 
+        if (Directory.Exists(folderName))
+        {
+            Directory.SetCurrentDirectory(folderName);
+            Console.WriteLine($"Folder '{folderName}' changed");
+            Console.WriteLine($"Current path \n {Directory.GetCurrentDirectory()}");
+        }
+        else
+        {
+            Console.WriteLine($"Folder '{folderName}' not created");
+        }
+    }
+    static public void ShowDir(string folderName)
+    {
+        Console.Write("Enter pattern --> ");
+        string pattern = Console.ReadLine()!;
+        if (Directory.Exists(folderName))
+        {
+            var entries = Directory.GetFileSystemEntries(folderName, pattern);
+            foreach (var e in entries)
+            {
+                FileInfo fi = new FileInfo(e);
+                string info = "<DIR>";
+                if (!fi.Attributes.HasFlag(FileAttributes.Directory))
+                    info = fi.Length.ToString();
+                Console.WriteLine($"{fi.CreationTime,-22} {fi.Name,-30}");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine($"Folder '{folderName}' not created");
+        }
+    }
+    static public void Conmand(string commandline)
+    {
+        var parts = commandline.Split(' ');
+        string command = parts[0].ToLower();
+        string argument = parts[1];
+
+        switch (command)
+        {
+            case "md":
+                CreateDIrectory(argument);
+                break;
+            case "rd":
+                DeleteDIrectory(argument);
+                break;
+            case "cd":
+                ChangeDirectory(argument);
+                break;
+            case "dir":
+                ShowDir(argument);
+                break;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        string ch = " ";
+        while (ch != "exit")
+        {
+            Console.Write(" > ");
+            ch = Console.ReadLine()!;
+            Conmand(ch);
+        }
+      
+    }
+}
