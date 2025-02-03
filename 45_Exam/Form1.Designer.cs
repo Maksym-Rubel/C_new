4dc36ccccc65d8f287e49ca4f4be4c9b0576e342
Maks <rubelmaksum2404@gmail.com>
2025-02-03 20:34:42 +0200

    new

---
+++ 45_Exam/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace _45_Exam
+{
+    partial class lblHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(0, 0);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(262, 89);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Search";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.23F);
+            this.textBox1.Location = new System.Drawing.Point(259, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(746, 48);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1262, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.16F);
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(0, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1262, 584);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1000, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 89);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnClickThis);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "lblHelloWorld";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.lblHelloWorld_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 45_Exam/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _45_Exam
+{
+   
+    public partial class lblHelloWorld : Form
+    {
+   
+        public lblHelloWorld()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            if (dir.Attributes != FileAttributes.Hidden)
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                foreach (DirectoryInfo d in dirs)
+                {
+                    listBox1.Items.Add(d);
+                }
+
+
+            }
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add($"{file.Name,-20}{file.LastWriteTime,-30}{file.CreationTime,-30}");
+            }
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true; 
+            }
+        }
+        private void textBox1_TextChanged(object sender, KeyEventArgs e) { }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+
+            textBox1.Text = Path.Combine(textBox1.Text,listBox1.SelectedItem.ToString());
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            if (dir.Attributes != FileAttributes.Hidden)
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                foreach (DirectoryInfo d in dirs)
+                {
+                    listBox1.Items.Add(d);
+                }
+
+
+            }
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add($"{file.Name,-20}{file.LastWriteTime,-30}{file.CreationTime,-30}");
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text[textBox1.Text.Length - 1] == '\\') 
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1,1);
+                while(textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+            else if(textBox1.Text[textBox1.Text.Length - 1] != '\\')
+            {
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            if (dir.Attributes != FileAttributes.Hidden)
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                foreach (DirectoryInfo d in dirs)
+                {
+                    listBox1.Items.Add(d);
+                }
+
+
+            }
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add($"{file.Name,-40}{file.LastWriteTime,-30}{file.CreationTime,-30}");
+            }
+
+        }
+
+        private void lblHelloWorld_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
