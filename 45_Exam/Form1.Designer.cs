00ff93fae04a7c2b17342361b91cc45a56e417b5
Maks <rubelmaksum2404@gmail.com>
2025-02-06 12:42:28 +0200

    Exam

--- 45_Exam/Form1.Designer.cs
+++ 45_Exam/Form1.Designer.cs
@@ -29,84 +29,180 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lblHelloWorld));
             this.btnClickThis = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
+            this.create = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.directoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnClickThis
             // 
-            this.btnClickThis.Location = new System.Drawing.Point(0, 0);
+            this.btnClickThis.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClickThis.BackgroundImage")));
+            this.btnClickThis.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnClickThis.Location = new System.Drawing.Point(58, 20);
             this.btnClickThis.Name = "btnClickThis";
-            this.btnClickThis.Size = new System.Drawing.Size(262, 89);
+            this.btnClickThis.Size = new System.Drawing.Size(55, 55);
             this.btnClickThis.TabIndex = 0;
-            this.btnClickThis.Text = "Search";
             this.btnClickThis.UseVisualStyleBackColor = true;
             this.btnClickThis.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.BackColor = System.Drawing.Color.LightGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.23F);
-            this.textBox1.Location = new System.Drawing.Point(259, 12);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(203, 20);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(746, 48);
+            this.textBox1.Size = new System.Drawing.Size(746, 41);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1262, 24);
-            this.menuStrip1.TabIndex = 3;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
             // listBox1
             // 
-            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.listBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.16F);
-            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 29;
-            this.listBox1.Location = new System.Drawing.Point(0, 86);
+            this.listBox1.Location = new System.Drawing.Point(0, 82);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1262, 584);
+            this.listBox1.Size = new System.Drawing.Size(1262, 580);
             this.listBox1.TabIndex = 4;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            this.listBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.copyToolStripMenuItem1,
+            this.pasteToolStripMenuItem,
+            this.copyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(127, 108);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.directoryToolStripMenuItem,
+            this.textFileToolStripMenuItem});
+            this.createToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("createToolStripMenuItem.Image")));
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(214, 26);
+            this.createToolStripMenuItem.Text = "Create";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem1
+            // 
+            this.copyToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("copyToolStripMenuItem1.Image")));
+            this.copyToolStripMenuItem1.Name = "copyToolStripMenuItem1";
+            this.copyToolStripMenuItem1.Size = new System.Drawing.Size(126, 26);
+            this.copyToolStripMenuItem1.Text = "Copy";
+            this.copyToolStripMenuItem1.Click += new System.EventHandler(this.copyToolStripMenuItem1_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("pasteToolStripMenuItem.Image")));
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(126, 26);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("copyToolStripMenuItem.Image")));
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(126, 26);
+            this.copyToolStripMenuItem.Text = "Delete";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1000, 0);
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(0, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(262, 89);
+            this.button1.Size = new System.Drawing.Size(55, 55);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Back";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // create
+            // 
+            this.create.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.create.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.15F);
+            this.create.Location = new System.Drawing.Point(591, 404);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(100, 34);
+            this.create.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Location = new System.Drawing.Point(131, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 55);
+            this.button3.TabIndex = 8;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // directoryToolStripMenuItem
+            // 
+            this.directoryToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("directoryToolStripMenuItem.Image")));
+            this.directoryToolStripMenuItem.Name = "directoryToolStripMenuItem";
+            this.directoryToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.directoryToolStripMenuItem.Text = "Directory";
+            this.directoryToolStripMenuItem.Click += new System.EventHandler(this.directoryToolStripMenuItem_Click);
+            // 
+            // textFileToolStripMenuItem
+            // 
+            this.textFileToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("textFileToolStripMenuItem.Image")));
+            this.textFileToolStripMenuItem.Name = "textFileToolStripMenuItem";
+            this.textFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.textFileToolStripMenuItem.Text = "Text file";
+            this.textFileToolStripMenuItem.Click += new System.EventHandler(this.textFileToolStripMenuItem_Click);
             // 
             // lblHelloWorld
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.create);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnClickThis);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "lblHelloWorld";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.lblHelloWorld_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,11 +211,18 @@
         #endregion
 
         private System.Windows.Forms.Button btnClickThis;
-        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem1;
+        private System.Windows.Forms.TextBox create;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem directoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textFileToolStripMenuItem;
     }
 }
 
--- 45_Exam/Form1.cs
+++ 45_Exam/Form1.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
@@ -20,12 +22,16 @@ namespace _45_Exam
         public lblHelloWorld()
         {
             InitializeComponent();
-            
+            create.Visible=false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || !Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("Invalid directory path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             listBox1.Items.Clear();
             DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
             if (dir.Attributes != FileAttributes.Hidden)
@@ -43,7 +49,9 @@ namespace _45_Exam
 
             foreach (FileInfo file in files)
             {
-                listBox1.Items.Add($"{file.Name,-20}{file.LastWriteTime,-30}{file.CreationTime,-30}");
+                listBox1.Items.Add(file);
+
+                
             }
 
         }
@@ -61,34 +69,47 @@ namespace _45_Exam
 
         private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-
-
-            textBox1.Text = Path.Combine(textBox1.Text,listBox1.SelectedItem.ToString());
-            listBox1.Items.Clear();
-            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
-            if (dir.Attributes != FileAttributes.Hidden)
+            if(Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "")
             {
-                DirectoryInfo[] dirs = dir.GetDirectories();
-
-                foreach (DirectoryInfo d in dirs)
+                textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+                listBox1.Items.Clear();
+                DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+                if (dir.Attributes != FileAttributes.Hidden)
                 {
-                    listBox1.Items.Add(d);
+                    DirectoryInfo[] dirs = dir.GetDirectories();
+
+                    foreach (DirectoryInfo d in dirs)
+                    {
+                        listBox1.Items.Add(d);
+                    }
+
+
                 }
+                FileInfo[] files = dir.GetFiles();
 
+                foreach (FileInfo file in files)
+                {
+                  
+                    listBox1.Items.Add(file);
 
+                }
             }
-            FileInfo[] files = dir.GetFiles();
-
-            foreach (FileInfo file in files)
+            else
             {
-                listBox1.Items.Add($"{file.Name,-20}{file.LastWriteTime,-30}{file.CreationTime,-30}");
+                Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString()));
             }
+
+            
 
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || !Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("Invalid directory path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             if (textBox1.Text[textBox1.Text.Length - 1] == '\\') 
             {
                 textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1,1);
@@ -123,7 +144,9 @@ namespace _45_Exam
 
             foreach (FileInfo file in files)
             {
-                listBox1.Items.Add($"{file.Name,-40}{file.LastWriteTime,-30}{file.CreationTime,-30}");
+                listBox1.Items.Add(file);
+
+                
             }
 
         }
@@ -131,6 +154,250 @@ namespace _45_Exam
         private void lblHelloWorld_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void listBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(MousePosition,ToolStripDropDownDirection.Right);
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string selectedPath = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+            if (Directory.Exists(selectedPath))
+            {
+                Directory.Delete(selectedPath, true); 
+                MessageBox.Show($"Папка '{listBox1.SelectedItem}' успішно видалена");
+            }
+       
+            else if (File.Exists(selectedPath))
+            {
+                File.Delete(selectedPath);
+                MessageBox.Show($"Файл '{listBox1.SelectedItem}' успішно видалений");
+            }
+            else
+            {
+                MessageBox.Show($"Елемент '{listBox1.SelectedItem}' не знайдено");
+            }
+          
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            if (dir.Attributes != FileAttributes.Hidden)
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                foreach (DirectoryInfo d in dirs)
+                {
+                    listBox1.Items.Add(d);
+                }
+
+
+            }
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add(file);
+
+
+            }
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+          
+        }
+        private string sourcePath = "";
+
+        private void copyToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem != null)
+            {
+                sourcePath = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+
+            }
+            
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        
+            string destinationPath = Path.Combine(textBox1.Text, Path.GetFileName(sourcePath));
+            
+            if (File.Exists(sourcePath))
+            {
+                File.Copy(sourcePath, destinationPath, true); 
+                
+            }
+            else if (Directory.Exists(sourcePath)) 
+            {
+                CopyDirectory(sourcePath, destinationPath);
+
+            }
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+            if (dir.Attributes != FileAttributes.Hidden)
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                foreach (DirectoryInfo d in dirs)
+                {
+                    listBox1.Items.Add(d);
+                }
+
+
+            }
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add(file);
+
+
+            }
+        }
+        private void CopyDirectory(string sourceDir, string destinationDir)
+        {
+            
+            Directory.CreateDirectory(destinationDir);
+
+           
+            foreach (string file in Directory.GetFiles(sourceDir))
+            {
+                string destFile = Path.Combine(destinationDir, Path.GetFileName(file));
+                File.Copy(file, destFile, true);
+            }
+
+          
+            foreach (string dir in Directory.GetDirectories(sourceDir))
+            {
+                string destDir = Path.Combine(destinationDir, Path.GetFileName(dir));
+                CopyDirectory(dir, destDir);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || !Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("Invalid directory path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            if (dir.Attributes != FileAttributes.Hidden)
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                foreach (DirectoryInfo d in dirs)
+                {
+                    listBox1.Items.Add(d);
+                }
+                
+
+            }
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                listBox1.Items.Add(file);
+
+
+            }
+        }
+
+        private void textFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            create.Location = this.PointToClient(Cursor.Position);
+            create.Visible = true;
+            create.KeyDown += (s, keyEvent) =>
+            {
+                if (keyEvent.KeyCode == Keys.Enter)
+                {
+                    create.Visible = false;
+                 
+                    keyEvent.SuppressKeyPress = true;
+                    create.Text += ".txt";
+                    string targetPath = Path.Combine(textBox1.Text, create.Text);
+                    MessageBox.Show(targetPath);
+                    File.Create(targetPath).Dispose();
+             
+                    listBox1.Items.Clear();
+                    DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+                    if (dir.Attributes != FileAttributes.Hidden)
+                    {
+                        DirectoryInfo[] dirs = dir.GetDirectories();
+
+                        foreach (DirectoryInfo d in dirs)
+                        {
+                            listBox1.Items.Add(d);
+                        }
+
+
+                    }
+                    FileInfo[] files = dir.GetFiles();
+
+                    foreach (FileInfo file in files)
+                    {
+                        listBox1.Items.Add(file);
+
+
+                    }
+
+                    create.Text = "";
+
+                }
+            };
+
+        }
+
+        private void directoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            create.Location = this.PointToClient(Cursor.Position);
+            create.Visible = true;
+            create.KeyDown += (s, keyEvent) =>
+            {
+                if (keyEvent.KeyCode == Keys.Enter)
+                {
+                    create.Visible = false;
+
+                    keyEvent.SuppressKeyPress = true;
+                    string targetPath = Path.Combine(textBox1.Text, create.Text);
+                    listBox1.Items.Add(Directory.CreateDirectory(targetPath));
+                    listBox1.Items.Clear();
+                    DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+                    if (dir.Attributes != FileAttributes.Hidden)
+                    {
+                        DirectoryInfo[] dirs = dir.GetDirectories();
+
+                        foreach (DirectoryInfo d in dirs)
+                        {
+                            listBox1.Items.Add(d);
+                        }
+
+
+                    }
+                    FileInfo[] files = dir.GetFiles();
+
+                    foreach (FileInfo file in files)
+                    {
+                        listBox1.Items.Add(file);
+
+
+                    }
+
+                    create.Text = "";
+
+                }
+            };
         }
     }
 }
